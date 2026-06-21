3c4c9076123f36f43e847f04a21a011131882b19
salmanmasroor <salmanmasroor07@gmail.com>
2026-06-21 16:26:30 +0500

    Inventory

---
+++ inventory.py
@@ -0,0 +1,28 @@
+import sqlite3
+from product import Product
+
+class Inventory:
+
+    def connect():
+        return sqlite3.connect("inventory.db")
+    
+    def add_product(self,product):
+        conn = self.connect()
+        cursor= conn.cursor()
+
+        cursor.execute("""
+        INSERT INTO PRODUCT (name,price, quantity) VALUES (?, ?, ?)
+        """,product.name,product.price,product.quantity)
+
+        conn.commit()
+        conn.close()
+    
+    def view_products(self):
+        conn = self.connect()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM products")
+        rows = cursor.fetchall()
+
+        conn.close()
+        return rows
