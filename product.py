ceb36db63aace5e151f44a355c050c6c42e6637f
salmanmasroor <salmanmasroor07@gmail.com>
2026-06-21 16:11:44 +0500

    structure of product

---
+++ product.py
@@ -0,0 +1,8 @@
+class Product:
+    def __init__(self,name,price,quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+    def __str__(self):
+        return f"{self.name} | Rs.{self.price} | Qty:{self.quantity}"
